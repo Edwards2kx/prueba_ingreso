7e4421a5e3d4b8d45e7f5b8774660aa4b115e34a
Edwards2kx <edwards2kx@gmail.com>
2022-10-27 22:00:01 -0500

    all Done

--- lib/ui/pages/user_post/user_post_page.dart
+++ lib/ui/pages/user_post/user_post_page.dart
@@ -1,56 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:prueba_ingreso/providers/post_providers.dart';
+import 'package:prueba_ingreso/ui/pages/user_post/widgets/post_card_widget.dart';
+import 'package:prueba_ingreso/ui/pages/user_post/widgets/sticky_user_widget.dart';
 import 'package:prueba_ingreso/ui/widgets/custom_appbar.dart';
+import 'package:prueba_ingreso/ui/widgets/refresh_button.dart';
 import '../../../data/entities/post.dart';
 import '../../../data/entities/user.dart';
 
-class PostsPage extends StatelessWidget {
+class PostsPage extends StatefulWidget {
   final User user;
   const PostsPage(this.user, {super.key});
 
+  @override
+  State<PostsPage> createState() => _PostsPageState();
+}
+
+class _PostsPageState extends State<PostsPage> {
   @override
   Widget build(BuildContext context) {
     final postProvider = Provider.of<PostProvider>(context);
     return Scaffold(
       appBar: customAppBar,
-      body: FutureBuilder(
-        future: postProvider.getPostsByUser(user.id),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          
-          if (snapshot.hasData) {
-            List<Post> postList = snapshot.data;
-            return Column(
-              children: [
-
-                Text(user.name),
-                Text(user.email),
-                Text(user.phone),
-
-                Expanded(
-                  child: ListView.builder(
-                    
-                    itemCount: postList.length,
-                    itemBuilder: (BuildContext context, int index) { 
-                    return Text(postList[index].title);
-                
-                   },),
-                ),
-              ],
-            );
-          } else {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('Cargando datos'),
-                  CircularProgressIndicator()
-                ],
-              ),
-            );
-          }
-        },
+      body: Column(
+        children: [
+          StickyUserContainer(widget.user),
+          Expanded(
+            child: FutureBuilder(
+              future: postProvider.getPostsByUser(widget.user.id),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                if (snapshot.hasData) {
+                  List<Post> postList = snapshot.data;
+                  return _buildPosts(postList, postProvider);
+                } else {
+                  return _loading();
+                }
+              },
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  Center _loading() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [Text('Cargando datos'), CircularProgressIndicator()],
+      ),
+    );
+  }
+
+  Widget _buildPosts(List<Post> postList, PostProvider postProvider) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: postList.isNotEmpty
+          ? ListView.builder(
+              itemCount: postList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return PostCard(postList[index]);
+              },
+            )
+          : RefreshButton(
+              callback: () async {
+                await postProvider.getPostsByUser(widget.user.id);
+                setState(() {});
+              },
+            ),
     );
   }
 }
